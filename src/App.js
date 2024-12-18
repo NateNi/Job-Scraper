96cca585675387658ee6d68ed230a52c9885cfb4
NateNi <nniesel90@gmail.com>
2024-12-17 19:22:39 -0600

    Break Mock Website element out into its own component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,8 @@ import "@fontsource/roboto/700.css";
 import "./App.css";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import RefreshIcon from "@mui/icons-material/Refresh";
-import HomeIcon from "@mui/icons-material/Home";
+import MockWebpage from "./MockWebpage.js";
 
 function App() {
   //const [currentTime, setCurrentTime] = useState(0);
@@ -101,97 +97,13 @@ function App() {
             </Paper>
           </Grid>
           <Grid key={2} item xs={12} md={6}>
-            <Paper
-              elevation={24}
-              sx={{
-                width: "80%",
-                margin: "20px auto",
-                borderRadius: 2,
-                overflow: "hidden",
-                border: "1px solid #ccc",
-              }}
-            >
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  padding: "8px 16px",
-                  borderBottom: "1px solid #ccc",
-                }}
-              >
-                <ArrowBackIcon sx={{ padding: "0.5rem" }} />
-                <ArrowForwardIcon sx={{ padding: "0.5rem" }} />
-                <RefreshIcon sx={{ padding: "0.5rem" }} />
-                <HomeIcon sx={{ padding: "0.5rem" }} />
-
-                <Box
-                  className={isURLInputFocused ? "focusedOutline" : ""}
-                  sx={{
-                    border: `2px solid ${isURLInputFocused ? "blue" : "#ccc"}`,
-                    transition: "border-color 0.5s ease",
-                    flexGrow: 1,
-                    mx: 2,
-                    padding: "0.5rem",
-                    paddingLeft: "1rem",
-                    borderRadius: 50,
-                  }}
-                >
-                  Enter URL
-                </Box>
-              </Box>
-
-              {/* Browser Content Area */}
-              <Box
-                sx={{
-                  height: "400px",
-                  padding: "3rem",
-                }}
-              >
-                <h2>
-                  Welcome to{" "}
-                  <span
-                    className={isCompanyInputFocused ? "focusedElement" : ""}
-                  >
-                    COMPANY
-                  </span>
-                </h2>
-                <h3>Job Results</h3>
-                {[...Array(3)].map((value, index) => (
-                  <Box
-                    key={value}
-                    sx={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                      padding: "10px",
-                      border: `1px solid ${
-                        isContainerInputFocused ? "blue" : "#ccc"
-                      }`,
-                      transition: "border-color 0.3s ease",
-                      borderRadius: "5px",
-                      marginBottom: "10px",
-                    }}
-                  >
-                    <h4
-                      className={
-                        (isTitleInputFocused ? "focusedElement" : "") +
-                        " mockWebElement"
-                      }
-                    >
-                      Job Title
-                    </h4>
-                    <p
-                      className={
-                        (isLinkInputFocused ? "focusedElement" : "") +
-                        " mockWebElement"
-                      }
-                    >
-                      <u>Click Here</u>
-                    </p>
-                  </Box>
-                ))}
-              </Box>
-            </Paper>
+            <MockWebpage
+              emphasizeURL={isURLInputFocused}
+              emphasizeCompany={isCompanyInputFocused}
+              emphasizeContainer={isContainerInputFocused}
+              emphasizeTitle={isTitleInputFocused}
+              emphasizeLink={isLinkInputFocused}
+            />
           </Grid>
         </Grid>
       </Paper>
---
+++ src/MockWebpage.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from "react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./App.css";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import HomeIcon from "@mui/icons-material/Home";
+
+export default function MockWebpage({
+  emphasizeURL,
+  emphasizeCompany,
+  emphasizeContainer,
+  emphasizeTitle,
+  emphasizeLink,
+}) {
+  return (
+    <Paper
+      elevation={24}
+      sx={{
+        margin: "20px auto",
+        borderRadius: 2,
+        overflow: "hidden",
+        border: "1px solid #ccc",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          padding: "8px 16px",
+          borderBottom: "1px solid #ccc",
+        }}
+      >
+        <ArrowBackIcon sx={{ padding: "0.5rem" }} />
+        <ArrowForwardIcon sx={{ padding: "0.5rem" }} />
+        <RefreshIcon sx={{ padding: "0.5rem" }} />
+        <HomeIcon sx={{ padding: "0.5rem" }} />
+
+        <Box
+          sx={{
+            border: `2px solid ${emphasizeURL ? "blue" : "#ccc"}`,
+            transition: "border-color 0.5s ease",
+            flexGrow: 1,
+            mx: 2,
+            padding: "0.5rem",
+            paddingLeft: "1rem",
+            borderRadius: 50,
+          }}
+        >
+          Enter URL
+        </Box>
+      </Box>
+
+      {/* Browser Content Area */}
+      <Box
+        sx={{
+          height: "400px",
+          padding: "3rem",
+        }}
+      >
+        <h2>
+          Welcome to{" "}
+          <span className={emphasizeCompany ? "focusedElement" : ""}>
+            COMPANY
+          </span>
+        </h2>
+        <h3>Job Results</h3>
+        {[...Array(3)].map((value, index) => (
+          <Box
+            key={value}
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              padding: "10px",
+              border: `${
+                emphasizeContainer ? "2px solid blue" : "1px solid #ccc"
+              }`,
+              transition: "all 0.3s ease",
+              borderRadius: "5px",
+              marginBottom: "10px",
+            }}
+          >
+            <h4
+              className={
+                (emphasizeTitle ? "focusedElement" : "") + " mockWebElement"
+              }
+            >
+              Job Title
+            </h4>
+            <p
+              className={
+                (emphasizeLink ? "focusedElement" : "") + " mockWebElement"
+              }
+            >
+              <u>Click Here</u>
+            </p>
+          </Box>
+        ))}
+      </Box>
+    </Paper>
+  );
+}
